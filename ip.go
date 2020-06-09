0778714e70a739b6f4b0f558cc4aff1f0f473dc5
liutianpeng <liutp@163.com>
2020-06-09 23:03:55 +0800

    ipip

---
+++ ip.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"github.com/playnb/util/log"
+	"net"
+)
+
+func GetIp(ifName string) string {
+	interfaces, err := net.Interfaces()
+	defaultIp := "127.0.0.1"
+	if err != nil {
+		return defaultIp
+	}
+
+	for _, i := range interfaces {
+		byName, err := net.InterfaceByName(i.Name)
+		if err != nil {
+			log.Trace("[Util] GetIp(%s) Error:%s", ifName, err.Error())
+			continue
+		}
+		addresses, err := byName.Addrs()
+		for _, v := range addresses {
+			if ipNet, ok := v.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+				if ipNet.IP.To4() != nil {
+					defaultIp = ipNet.IP.String()
+					if byName.Name == ifName {
+						return ipNet.IP.String()
+					}
+				}
+			}
+		}
+	}
+	return defaultIp
+}
